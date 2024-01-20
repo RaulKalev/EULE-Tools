afe52b91608b7a3f59067f94f3c9f00bd70eaf3f
Raul Kalev <mibilit@me.com>
2024-01-20 18:56:25 +0200

    toolbar added

---
+++ Tööriistad.tab/Joonestamine.Panel/Draw Line.pushbutton/script.py
@@ -0,0 +1,203 @@
+# -*- coding: utf-8 -*-
+__title__ = "Draw Line"  # Name of the button displayed in Revit
+__author__ = "Raul Kalev"
+__version__ = 'Version: 0.1'
+__doc__ = """
+Select 2 detail items and draw a detail line between those 2 items
+"""
+
+
+import clr
+clr.AddReference('System.Windows.Forms')
+clr.AddReference('System.Drawing')
+clr.AddReference('RevitAPI')
+clr.AddReference('RevitAPIUI')
+
+from Autodesk.Revit.UI import TaskDialog
+from Autodesk.Revit.DB import XYZ
+from System.Drawing import Size, Point, Font, FontStyle
+from System.Windows.Forms import Application, Button, Form, FormWindowState
+from Autodesk.Revit.UI.Selection import ObjectType
+from Autodesk.Revit.DB import Line, Transaction
+from Autodesk.Revit.DB import GraphicsStyle, FilteredElementCollector, BuiltInCategory
+from Autodesk.Revit.DB import GraphicsStyle, FilteredElementCollector, CategoryType, BuiltInCategory, Transaction, LinePatternElement
+from System.Windows.Forms import ComboBox
+
+def get_line_styles(doc):
+    line_styles = {}
+
+    # Fetching all elements of type GraphicsStyle
+    collector = FilteredElementCollector(doc).OfClass(GraphicsStyle)
+
+    for elem in collector:
+        # Filtering for elements that are line styles (not patterns or materials)
+        if elem.GraphicsStyleCategory != None and elem.GraphicsStyleCategory.CategoryType == CategoryType.Annotation:
+            line_styles[elem.Name] = elem.Id
+
+    return line_styles
+
+
+class SimpleForm(Form):
+    def __init__(self, uidoc):
+        self.uidoc = uidoc
+        self.Text = 'Draw Lines'
+        self.Size = Size(250, 550)
+        self.midpoint_location = None
+
+        # Custom font and size for buttons
+        buttonFont = Font("Arial", 12, FontStyle.Bold)
+        buttonSize = Size(150, 75)  # Uniform size for all buttons
+        
+        # Variables to store element locations
+        self.element1_location = None
+        self.element2_location = None
+
+        # Button for selecting Element 1
+        self.button1 = Button()
+        self.button1.Text = 'Select Element 1'
+        self.button1.Font = buttonFont
+        self.button1.Size = buttonSize
+        self.button1.Location = Point(10, 30)
+        self.button1.Click += self.on_button1_click
+        self.Controls.Add(self.button1)
+
+        # Button for selecting Element 2
+        self.button2 = Button()
+        self.button2.Text = 'Select Element 2'
+        self.button2.Font = buttonFont
+        self.button2.Size = buttonSize
+        self.button2.Location = Point(10, 105)
+        self.button2.Click += self.on_button2_click
+        self.Controls.Add(self.button2)
+
+        # Button to display stored values
+        self.button3 = Button()
+        self.button3.Text = 'Show Stored Values'
+        self.button3.Font = buttonFont
+        self.button3.Size = buttonSize
+        self.button3.Location = Point(10, 180)
+        self.button3.Click += self.on_button3_click
+        self.Controls.Add(self.button3)
+        
+        
+       # Add this new button in your __init__ method
+        self.button4 = Button()
+        self.button4.Text = 'Draw Detail Line'
+        self.button4.Font = buttonFont
+        self.button4.Size = buttonSize
+        self.button4.Location = Point(10, 255)
+        self.button4.Click += self.on_button4_click
+        self.Controls.Add(self.button4)
+
+        self.line_styles = get_line_styles(uidoc.Document)
+        self.dropdown = ComboBox()
+        self.dropdown.Location = Point(10, 330)
+        self.dropdown.Size = Size(150, 20)
+            # Sort the line style names alphabetically and add them to the dropdown
+        for name in sorted(self.line_styles.keys()):
+            self.dropdown.Items.Add(name)
+        self.Controls.Add(self.dropdown)
+
+    def on_button1_click(self, sender, args):
+        self.WindowState = FormWindowState.Minimized
+        self.element1_location = self.select_element_and_get_location()
+        self.WindowState = FormWindowState.Normal
+        self.update_midpoint()
+
+    def on_button2_click(self, sender, args):
+        self.WindowState = FormWindowState.Minimized
+        self.element2_location = self.select_element_and_get_location()
+        self.WindowState = FormWindowState.Normal
+        self.update_midpoint()
+
+    def on_button3_click(self, sender, args):
+        self.WindowState = FormWindowState.Minimized
+        element1_msg = self.format_location(self.element1_location)
+        element2_msg = self.format_location(self.element2_location)
+        midpoint_msg = self.format_location(self.midpoint_location) if self.midpoint_location else 'Not Set'
+
+        message = 'Element 1 Location: {}\nElement 2 Location: {}\nMidpoint Location: {}'.format(
+            element1_msg, element2_msg, midpoint_msg)
+        TaskDialog.Show('Stored Values', message)
+        self.WindowState = FormWindowState.Normal
+
+    def select_element_and_get_location(self):
+        try:
+            selected_ref = self.uidoc.Selection.PickObject(ObjectType.Element)
+            if selected_ref is not None:
+                element = self.uidoc.Document.GetElement(selected_ref)
+                if hasattr(element, 'Location') and element.Location.Point:
+                    location = element.Location.Point
+                    location_point = XYZ(location.X, location.Y, location.Z)
+                    return location_point
+                else:
+                    return None
+        except Exception as e:
+            return None
+
+    def format_location(self, location):
+        if location:
+            return 'X: {:.2f}, Y: {:.2f}, Z: {:.2f}'.format(location.X, location.Y, location.Z)
+        else:
+            return 'Not Set'
+        
+    def update_midpoint(self):
+        if self.element1_location and self.element2_location:
+            mid_x = (self.element1_location.X + self.element2_location.X) / 2.0
+            mid_y = (self.element1_location.Y + self.element2_location.Y) / 2.0
+            self.midpoint_location = XYZ(mid_x, mid_y, 0)  # Assuming Z = 0 for simplicity
+        
+    def on_button4_click(self, sender, args):
+        self.draw_detail_line()
+    
+    def on_button_show_midpoint_click(self, sender, args):
+        if self.midpoint_location:
+            message = 'Midpoint Location: X: {:.2f}, Y: {:.2f}, Z: {:.2f}'.format(
+                self.midpoint_location.X, self.midpoint_location.Y, self.midpoint_location.Z)
+            TaskDialog.Show('Midpoint', message)
+        else:
+            TaskDialog.Show('Midpoint', 'Midpoint is not set yet.')
+    
+    def draw_detail_line(self):
+        selected_style_name = self.dropdown.SelectedItem
+        selected_style_id = self.line_styles.get(selected_style_name)
+
+        if selected_style_id and self.element1_location and self.element2_location:
+            self.update_midpoint()
+            midpoint = self.midpoint_location
+
+            if midpoint:
+                with Transaction(self.uidoc.Document, 'Draw Detail Line') as trans:
+                    trans.Start()
+                    try:
+                        # Define the points for line segments
+                        point1 = XYZ(self.element1_location.X, self.element1_location.Y, 0)
+                        interm_point1 = XYZ(midpoint.X, self.element1_location.Y, 0)
+                        interm_point2 = XYZ(midpoint.X, self.element2_location.Y, 0)
+                        point2 = XYZ(self.element2_location.X, self.element2_location.Y, 0)
+
+                        # Create line geometries
+                        line1_geometry = Line.CreateBound(point1, interm_point1)
+                        line2_geometry = Line.CreateBound(interm_point1, interm_point2)
+                        line3_geometry = Line.CreateBound(interm_point2, point2)
+
+                        # Set line style for each detail curve and create them
+                        for line_geom in [line1_geometry, line2_geometry, line3_geometry]:
+                            line = self.uidoc.Document.Create.NewDetailCurve(self.uidoc.ActiveView, line_geom)
+                            line.LineStyle = self.uidoc.Document.GetElement(selected_style_id)
+
+                        trans.Commit()
+                    except Exception as e:
+                        TaskDialog.Show('Error', 'Failed to draw line: ' + str(e))
+                        trans.RollBack()
+            else:
+                TaskDialog.Show('Error', 'Midpoint not set.')
+        else:
+            TaskDialog.Show('Error', 'Please select both elements and a line style.')
+
+
+
+# To run this form, you need to pass the UIDocument from a RevitPythonShell script
+uidoc = __revit__.ActiveUIDocument
+form = SimpleForm(uidoc)
+Application.Run(form)
---
+++ Tööriistad.tab/Sheets.Panel/Change Revisions.pushbutton/script.py
@@ -0,0 +1,201 @@
+# -*- coding: utf-8 -*-
+__title__ = "Change Revision"  # Name of the button displayed in Revit
+__author__ = "Raul Kalev"
+__version__ = 'Version: 0.1'
+__doc__ = """
+Add or remove a revision from selected sheets
+"""
+
+import clr
+import System
+clr.AddReference('System.Windows.Forms')
+clr.AddReference('System.Drawing')
+clr.AddReference('RevitAPI')
+clr.AddReference('RevitAPIUI')
+
+from System.Windows.Forms import Form, ListView, View, Button, ColumnHeader, HorizontalAlignment, ListViewItem, ComboBox, Label, Application, ButtonBase, ControlPaint, Padding, FlatStyle
+from System.Drawing import Size, Point, Color, Pen, Drawing2D, Graphics, Rectangle, Font, FontStyle
+from Autodesk.Revit.DB import FilteredElementCollector, BuiltInCategory, ElementId, Transaction
+from Autodesk.Revit.UI import TaskDialog
+
+
+class SheetSelectorForm(Form):
+    def __init__(self, sheets, doc):
+        self.Text = "Select Sheets"
+        self.Size = Size(415, 650)
+        self.doc = doc
+
+         # Variables for color controls
+        backgroundColor1 = Color.FromArgb(24, 24, 24)
+        backgroundColor2 = Color.FromArgb(31, 31, 31)
+        textColor1 = Color.FromArgb(255, 255, 255)
+        buttonColor1 = Color.FromArgb(66, 66, 66)
+        font1 = Font("Arial", 12,)
+
+        #self.BackColor = backgroundColor1
+
+        self.listView = ListView()
+        self.listView.View = View.Details
+        self.listView.CheckBoxes = True
+        self.listView.FullRowSelect = True
+        self.listView.Columns.Add("Sheet Name", -2, HorizontalAlignment.Left)
+        self.listView.Columns.Add("Sheet Number", -2, HorizontalAlignment.Left)
+        self.listView.Columns.Add("Current Revision", -2, HorizontalAlignment.Left)
+        self.listView.Size = Size(380, 500)
+        self.listView.Location = Point(10, 10)
+        #self.listView.BackColor = backgroundColor2
+        #self.listView.ForeColor = textColor1
+
+        for sheet in sheets:
+            item = ListViewItem(sheet.Name)
+            item.SubItems.Add(sheet.SheetNumber)
+            item.Tag = sheet.Id  # Store the sheet ID in the tag property
+            current_revision = self.get_latest_revision(sheet)
+            item.SubItems.Add(current_revision)
+            self.listView.Items.Add(item)
+
+        self.revisionLabel = Label()
+        self.revisionLabel.Text = "Select Revision:"
+        self.revisionLabel.Font = font1
+        self.revisionLabel.Location = Point(10, 520)
+        self.revisionLabel.Size = Size(130, 20)
+        #self.revisionLabel.ForeColor = textColor1
+        self.Controls.Add(self.revisionLabel)
+
+        self.revisionComboBox = ComboBox()
+        self.revisionComboBox.Location = Point(140, 520)
+        self.revisionComboBox.Size = Size(200, 20)
+        #self.revisionComboBox.BackColor = backgroundColor1
+        #self.revisionComboBox.ForeColor = textColor1
+        self.load_revisions(doc)
+        self.Controls.Add(self.revisionComboBox)
+
+        self.applyButton = Button()
+        self.applyButton.Text = "Apply"
+        self.applyButton.Location = Point(300, 560)
+        self.applyButton.Click += self.OnApply
+        #self.applyButton.FlatStyle = FlatStyle.Flat
+        #self.applyButton.FlatAppearance.BorderSize = 0
+        #self.applyButton.BackColor = buttonColor1
+        #self.applyButton.ForeColor = textColor1
+        self.Controls.Add(self.applyButton)
+        
+        self.removeRevisionButton = Button()
+        self.removeRevisionButton.Text = "Remove"
+        self.removeRevisionButton.Location = Point(220, 560)
+        self.removeRevisionButton.Click += self.OnRemoveRevision
+        #self.removeRevisionButton.FlatStyle = FlatStyle.Flat
+        #self.removeRevisionButton.FlatAppearance.BorderSize = 0
+        #self.removeRevisionButton.BackColor = buttonColor1
+        #self.removeRevisionButton.ForeColor = textColor1
+        self.Controls.Add(self.removeRevisionButton)
+
+        self.selectAllButton = Button()
+        self.selectAllButton.Text = "Select All"
+        self.selectAllButton.Location = Point(10, 560)
+        self.selectAllButton.Click += self.OnSelectAll
+        #self.selectAllButton.FlatStyle = FlatStyle.Flat
+        #self.selectAllButton.FlatAppearance.BorderSize = 0
+        #self.selectAllButton.BackColor = buttonColor1
+        #self.selectAllButton.ForeColor = textColor1
+        self.Controls.Add(self.selectAllButton)
+
+        self.deselectAllButton = Button()
+        self.deselectAllButton.Text = "Deselect All"
+        self.deselectAllButton.Location = Point(90, 560)
+        self.deselectAllButton.Click += self.OnDeselectAll
+        #self.deselectAllButton.FlatStyle = FlatStyle.Flat
+        #self.deselectAllButton.FlatAppearance.BorderSize = 0
+        #self.deselectAllButton.BackColor = buttonColor1
+        #self.deselectAllButton.ForeColor = textColor1
+        self.Controls.Add(self.deselectAllButton)
+
+        self.Controls.Add(self.listView)
+
+    def load_revisions(self, doc):
+        revisions = FilteredElementCollector(doc).OfCategory(BuiltInCategory.OST_Revisions).WhereElementIsNotElementType().ToElements()
+        for rev in revisions:
+            self.revisionComboBox.Items.Add(rev.Name)
+
+    def get_latest_revision(self, sheet):
+        revs = sheet.GetAllRevisionIds()
+        if revs.Count > 0:
+            latest_rev = max([self.doc.GetElement(rev_id).RevisionNumber for rev_id in revs])
+            return latest_rev
+        else:
+            return "No Revision"
+
+    def OnApply(self, sender, args):
+        selected_revision_name = self.revisionComboBox.SelectedItem
+        selected_revision_id = None
+
+        revisions = FilteredElementCollector(self.doc).OfCategory(BuiltInCategory.OST_Revisions).WhereElementIsNotElementType().ToElements()
+        for rev in revisions:
+            if rev.Name == selected_revision_name:
+                selected_revision_id = rev.Id
+                break
+
+        if selected_revision_id is None:
+            TaskDialog.Show("Error", "Selected revision not found in document.")
+            return
+
+        selected_sheet_ids = [item.Tag for item in self.listView.Items if item.Checked]
+
+        transaction = Transaction(self.doc)
+        transaction.Start("Apply Revision")
+        for sheet_id in selected_sheet_ids:
+            sheet = self.doc.GetElement(sheet_id)
+            current_revs = sheet.GetAllRevisionIds()
+            if selected_revision_id not in current_revs:
+                current_revs.Add(selected_revision_id)
+                sheet.SetAdditionalRevisionIds(current_revs)
+        transaction.Commit()
+
+        TaskDialog.Show("Success", "Applied revision '{}' to {} sheets".format(selected_revision_name, len(selected_sheet_ids)))
+        self.Close()
+
+    def OnRemoveRevision(self, sender, args):
+        selected_revision_name = self.revisionComboBox.SelectedItem
+        selected_revision_id = None
+
+        revisions = FilteredElementCollector(self.doc).OfCategory(BuiltInCategory.OST_Revisions).WhereElementIsNotElementType().ToElements()
+        for rev in revisions:
+            if rev.Name == selected_revision_name:
+                selected_revision_id = rev.Id
+                break
+
+        if selected_revision_id is None:
+            TaskDialog.Show("Error", "Selected revision not found in document.")
+            return
+
+        selected_sheet_ids = [item.Tag for item in self.listView.Items if item.Checked]
+
+        transaction = Transaction(self.doc)
+        transaction.Start("Remove Revision")
+        for sheet_id in selected_sheet_ids:
+            sheet = self.doc.GetElement(sheet_id)
+            current_revs = sheet.GetAllRevisionIds()
+            if selected_revision_id in current_revs:
+                current_revs.Remove(selected_revision_id)
+                sheet.SetAdditionalRevisionIds(current_revs)
+        transaction.Commit()
+
+        TaskDialog.Show("Success", "Removed revision '{}' from {} sheets".format(selected_revision_name, len(selected_sheet_ids)))
+
+        self.Close()  # Add this line to close the form
+
+    def OnSelectAll(self, sender, args):
+        for item in self.listView.Items:
+            item.Checked = True
+
+    def OnDeselectAll(self, sender, args):
+        for item in self.listView.Items:
+            item.Checked = False
+
+def show_sheet_selector():
+    doc = __revit__.ActiveUIDocument.Document
+    sheets = FilteredElementCollector(doc).OfCategory(BuiltInCategory.OST_Sheets).WhereElementIsNotElementType().ToElements()
+    form = SheetSelectorForm(sheets, doc)
+    form.ShowDialog()  # This line is changed
+
+show_sheet_selector()
